7f2aeb8ea47484c8028902b2b618b0dd9468efa3
LucasMagenis <lucas.magenis@hotmail.com>
2025-10-10 21:35:01 -0300

    NMP + JS

--- clock.js
+++ clock.js
@@ -1,12 +1,18 @@
 
+const timer = new easytimer.Timer();
 const clock = document.getElementById('clock');
+
+timer.addEventListener('secondsUpdated', () => {
+  clock.textContent = timer.getTimeValues().toString(['minutes', 'seconds']);
+});
 
 const botaoPomodoro = document.getElementById("pomodoro");
 const botaoPausaCurta = document.getElementById("pausaCurta");
 const botaoPausaLonga = document.getElementById("pausaLonga");
+
 const botaoFinalizar = document.getElementById("finalizar");
 const botaoIniciar = document.getElementById("iniciar");
-const botaoParar = document.getElementById("parar");
+const botaoPausar = document.getElementById("pausar");
 
 const botoes = document.querySelectorAll('.botoes');
 
@@ -15,6 +21,32 @@ function limpa(){
     bt.classList.remove('btn-active');
   }
 }
+
+botaoIniciar.addEventListener('click', () => {
+  timer.start({ countdown: true, startValues: { minutes: 25} });
+  botaoIniciar.classList.toggle('hidden');
+  botaoPausar.classList.toggle('hidden');
+})
+
+botaoPausar.addEventListener('click', () => {
+  timer.pause()
+  botaoIniciar.classList.toggle('hidden')
+  botaoPausar.classList.toggle('hidden')
+})
+
+botaoFinalizar.addEventListener('click', () => {
+  timer.stop()
+  botaoIniciar.classList.toggle('hidden');
+  botaoPausar.classList.toggle('hidden');
+  botaoPomodoro.click();
+})
+
+
+// botaoFinalizar.addEventListener('click', () => {
+//   clock.textContent = "00:00";
+//   limpa();
+//   botaoFinalizar.classList.add('btn-active');
+// })
 
 botaoPomodoro.addEventListener('click', () => {
   clock.textContent = "25:00";
@@ -34,11 +66,11 @@ botaoPausaLonga.addEventListener('click', () => {
   botaoPausaLonga.classList.add('btn-active');
 })
 
-botaoFinalizar.addEventListener('click', () => {
-  clock.textContent = "00:00";
-  limpa();
-  botaoFinalizar.classList.add('btn-active');
-})
+// botaoFinalizar.addEventListener('click', () => {
+//   clock.textContent = "00:00";
+//   limpa();
+//   botaoFinalizar.classList.add('btn-active');
+// })
 
 const lista = document.getElementById("lista");
 const botaoNova = document.getElementById("nova");
